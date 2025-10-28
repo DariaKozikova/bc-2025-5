57dd58fb7dd607919757b6bf7de161f8498005bc
Daria bc2024-5 <kozikovadasa@gmail.com>
2025-10-28 19:17:50 +0200

    the first part is done

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+const http = require('http');
 const { program } = require('commander');
+const fs = require('fs');
+
+program
+  .option('-h, --host <host>')
+  .option('-p, --port <port>')
+  .option('-c, --cache <path>');
+
+program.parse();
+const options = program.opts();
+
+if (!options.host || !options.port || !options.cache) {
+  console.log('Помилка: потрібно вказати --host, --port і --cache');
+  process.exit(1);
+}
+
+const server = http.createServer(function (req, res) {
+  res.end('Server is running');
+});
+
+server.listen(options.port, options.host, function () {
+  console.log(`Сервер запущено на адресі: http://${options.host}:${options.port}`);
+});
